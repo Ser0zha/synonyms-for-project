df0935b137c66f39d8416fcfbf4e6b4441839681
TheFattestCatProg <shvyrov2005@gmail.com>
2024-11-10 19:22:43 +0300

    Теперь можно тренировать модель

---
+++ dataset_tokenizer.py
@@ -0,0 +1,46 @@
+from datasets import Dataset
+from transformers import T5Tokenizer
+import json
+
+
+MODEL_NAME = "ai-forever/rut5-base"
+DATASET_PATH = "datasets/data_ru_lang.json"
+OUTPUT_PATH = "datasets/data_ru_lang.dataset"
+
+
+def load_dataset() -> Dataset:
+    with open(DATASET_PATH, 'r') as file:
+        data = json.load(file)
+    
+    return Dataset.from_list([
+        {
+            'input': 'IS IT CORRECT TO PUT WORD "{i["word"]}" INTO SENTENCE: {i["text"].replace("WORD", "<extra_id_0>")}', 
+            'target': 'yes' if i['correct'] else 'no'
+        }
+        for i in data
+    ])
+
+
+def tokenized(data: Dataset, tokenizer) -> Dataset:
+    def f(batch):
+        inputs = tokenizer(batch['input'], max_length=512, truncation=True, padding=True, return_tensors='pt')
+        labels = tokenizer(batch['target'], max_length=3, truncation=True, padding=True, return_tensors='pt')
+        return {
+            'input_ids': inputs.input_ids,
+            'attention_mask': inputs.attention_mask,
+            'labels': labels.input_ids
+        }
+
+    return data.map(f, batched=True)
+
+
+def main():
+    tokenizer = T5Tokenizer.from_pretrained(MODEL_NAME, legacy=False)
+
+    dataset = load_dataset()
+    dataset = tokenized(dataset, tokenizer)
+    dataset.save_to_disk(OUTPUT_PATH)
+
+
+if __name__ == '__main__':
+    main()
---
+++ train.py
@@ -0,0 +1,36 @@
+from transformers import T5ForConditionalGeneration
+from transformers import TrainingArguments, Trainer
+
+from datasets import Dataset
+
+
+MODEL_NAME = "ai-forever/rut5-base"
+DATASET_PATH = "datasets/data_ru_lang.dataset"
+
+
+def main():
+    model = T5ForConditionalGeneration.from_pretrained(MODEL_NAME)
+
+    dataset = Dataset.load_from_disk(DATASET_PATH)
+
+    training_args = TrainingArguments(
+        output_dir="./train_results",
+        per_device_train_batch_size=128,  # Подбирайте в зависимости от памяти GPU
+        gradient_accumulation_steps=2,
+        num_train_epochs=3,
+        logging_dir="./train_logs",
+        weight_decay=0.01,
+        save_total_limit=2,
+        fp16=True,  # Включить, если используете GPU NVIDIA
+    )
+
+    trainer = Trainer(
+        model=model,
+        args=training_args,
+        train_dataset=dataset,
+    )
+    print(trainer.train())
+
+
+if __name__ == "__main__":
+    main()
